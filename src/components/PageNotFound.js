f43fbb396b5aaf652ea7a7c44d5e45d062be8cc4
ravikantpal1408 <ravikantpal123@gmail.com>
2020-04-16 21:34:19 +0530

    done creating basic navigation schema and component layout structure

---
+++ src/components/PageNotFound.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const PageNotFound = () => {
+  return (
+    <div>
+      <h1>Opps! Page not found</h1>
+    </div>
+  );
+};
+export default PageNotFound;
---
+++ src/components/courses/CoursePage.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+
+class CoursePage extends Component {
+  render() {
+    return (
+      <div>
+        <h2>Course Page</h2>
+      </div>
+    );
+  }
+}
+export default CoursePage;
