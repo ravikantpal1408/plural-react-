d5ab48f23ecb336d3eb4ed03c418a9818cc1515c
ravikantpal1408 <ravikantpal123@gmail.com>
2020-04-19 17:18:36 +0530

    wrote first jest test

--- src/redux/actions/courseActions.js
+++ src/redux/actions/courseActions.js
@@ -1,6 +1,6 @@
-import * as types from "./actionTypes";
-import * as courseApi from "../../api/courseApi";
-import { beginApiCall, apiCallError } from "./apiStatusActions";
+import * as types from './actionTypes';
+import * as courseApi from '../../api/courseApi';
+import { beginApiCall, apiCallError } from './apiStatusActions';
 
 export function loadCourseSuccess(courses) {
   return { type: types.LOAD_COURSES_SUCCESS, courses };
@@ -18,33 +18,37 @@ export function deleteCourseOptimistic(course) {
   return { type: types.DELETE_COURSE_OPTIMISTIC, course };
 }
 
+/* 👆 Action Creator Funcitons 👆  */
+/* 👇 Thunk Creator Funcitons 👇  */
+
 export function loadCourses() {
-  return function(dispatch) {
+  return function (dispatch) {
     dispatch(beginApiCall());
     return courseApi
       .getCourses()
-      .then(courses => {
+      .then((courses) => {
         dispatch(loadCourseSuccess(courses));
       })
-      .catch(error => {
+      .catch((error) => {
         dispatch(apiCallError(error));
         throw error;
       });
   };
 }
 
-export function saveCourse(course) {
+export function saveCourse(course, getState) {
+  console.log('setting state : ', getState);
   //eslint-disable-next-line no-unused-vars
-  return function(dispatch, getState) {
+  return function (dispatch, getState) {
     dispatch(beginApiCall());
     return courseApi
       .saveCourse(course)
-      .then(savedCourse => {
+      .then((savedCourse) => {
         course.id
           ? dispatch(updateCourseSuccess(savedCourse))
           : dispatch(createCourseSuccess(savedCourse));
       })
-      .catch(error => {
+      .catch((error) => {
         dispatch(apiCallError(error));
         throw error;
       });
@@ -52,7 +56,7 @@ export function saveCourse(course) {
 }
 
 export function deleteCourse(course) {
-  return function(dispatch) {
+  return function (dispatch) {
     // Doing optimistic delete, so not dispatching begin/end api call
     // actions, or apiCallError action since we're not showing the loading status for this.
     dispatch(deleteCourseOptimistic(course));
