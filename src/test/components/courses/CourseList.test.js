54fdb4fa2d7c962f49be6c0060449b8c31435ef3
ravikantpal1408 <ravikantpal123@gmail.com>
2020-04-24 00:19:07 +0530

    testing dynamic tables

--- src/test/components/courses/CourseList.test.js
+++ src/test/components/courses/CourseList.test.js
@@ -1,21 +1,26 @@
 import React from 'react';
 import CourseList from '../../../components/courses/CourseList';
-import { shallow, mount, render } from 'enzyme';
-// import renderer from 'react-test-renderer';
+import {shallow} from 'enzyme';
 import MockData from '../../../../tools/mockData';
+import {Provider} from 'react-redux';
+import configureStore from "../../../redux/configureStore";
+
+
+const store = configureStore();
+
 
 const props = MockData.courses;
 
 test('should match the snapshot', () => {
-  const mockDelete = jest.fn();
-  const wrapper = shallow(
-    <CourseList courses={props} onDeleteClick={mockDelete} />
-  );
-  expect(wrapper).toMatchSnapshot();
+    // const mockDelete = jest.fn();
+    const wrapper = shallow(<Provider store={store}>
+        <CourseList courses={props} onDeleteClick={jest.fn()}/>
+    </Provider>);
+    expect(wrapper).toMatchSnapshot();
 });
 
 test('should delete on click', () => {
-  const wrapper = shallow(<CourseList courses={props} />);
-  const instance = wrapper.find('.btn-outline-danger').at(1);
-  console.log(instance);
+    const wrapper = shallow(<CourseList courses={props} onDeleteClick={jest.fn()}/>);
+    const instance = wrapper.find('.btn-outline-danger').at(1);
+    console.log(instance);
 });
